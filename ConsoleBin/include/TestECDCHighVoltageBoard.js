48be3f71826453daeb57876a5c7bc934e0c2107b
Mgainullin <m.gainullin@proton-electrotex.com>
2020-10-26 14:22:56 +0200

    Добавил функцию калибровки ячеек

--- ConsoleBin/include/TestECDCHighVoltageBoard.js
+++ ConsoleBin/include/TestECDCHighVoltageBoard.js
@@ -1,5 +1,28 @@
 include("PrintStatus.js")
 
+function ECDC_HV_CalCell(Voltage, CellNumder)
+{
+	if(dev.r(192) == 3)
+	{
+		dev.w(128, Voltage);
+		dev.w(151, CellNumder);
+		
+		dev.c(150);
+		
+		sleep(1000);
+		
+		if(dev.r(192) == 1)
+			PrintStatus();
+	}
+	else
+	{
+		if(dev.r(192) != 1)
+			print("Device not ready");
+		else
+			PrintStatus();
+	}
+}
+
 function ECDC_HV_Measure(Voltage, Current)
 {
 	if(dev.r(192) == 3)
