d4a909cad3660d9582ecb4cf21838d29af6cd3ce
asytyi <alexander.a.sytyi@gmail.com>
2020-10-27 17:39:11 +0300

    Добавлены функции чтения и записи 32-битных значений ECCB и чтение статуса ноды

--- ConsoleBin/include/TestECCB.js
+++ ConsoleBin/include/TestECCB.js
@@ -30,6 +30,41 @@ function ECCB_ReadRegS(NodeID, Reg)
 	}
 }
 
+function ECCB_ReadReg32(NodeID, RegL, RegH)
+{
+	var result = 0
+	
+	dev.w(185, NodeID)
+	dev.w(186, RegL)
+	dev.c(10)
+	
+	if (dev.r(230) == 0)
+	{
+		result |= dev.r(231)
+	}
+	else
+	{
+		print('Read low part error.')
+		print("Err code: " + dev.r(230))
+		return
+	}
+	
+	dev.w(186, RegH)
+	dev.c(10)
+	
+	if (dev.r(230) == 0)
+	{
+		result |= dev.r(231) << 16
+	}
+	else
+	{
+		print('Read high part error.')
+		print("Err code: " + dev.r(230))
+	}
+	
+	return result
+}
+
 function ECCB_WriteReg(NodeID, Reg, Value)
 {
 	dev.w(185, NodeID)
@@ -50,6 +85,31 @@ function ECCB_WriteRegS(NodeID, Reg, Value)
 	
 	if (dev.r(230) != 0)
 		print("Err code: " + dev.r(230))
+}
+
+function ECCB_WriteReg32(NodeID, RegL, RegH, Value)
+{
+	dev.w(185, NodeID)
+	dev.w(186, RegL)
+	dev.w(187, Value & 0xffff)
+	dev.c(11)
+	
+	if (dev.r(230) != 0)
+	{
+		print('Write low part error.')
+		print("Err code: " + dev.r(230))
+		return
+	}
+	
+	dev.w(186, RegH)
+	dev.w(187, (Value >> 16) & 0xffff)
+	dev.c(11)
+	
+	if (dev.r(230) != 0)
+	{
+		print('Write high part error.')
+		print("Err code: " + dev.r(230))
+	}
 }
 
 function ECCB_Call(NodeID, Action)
@@ -82,6 +142,17 @@ function ECCB_Status()
 	print("Function: 		" + dev.r(227))
 	print("Error: 			" + dev.r(225))
 	print("ExtData:		" + dev.r(228))
+}
+
+function ECCB_NodeStatus(Node)
+{
+	print("Registers [192 - 197]");
+	print("Device state:	" + ECCB_ReadReg(Node, 192));
+	print("Fault reason:	" + ECCB_ReadReg(Node, 193));
+	print("Disable reason:	" + ECCB_ReadReg(Node, 194));
+	print("Warning:	" + ECCB_ReadReg(Node, 195));
+	print("Problem:	" + ECCB_ReadReg(Node, 196));
+	print("OpResult:	" + ECCB_ReadReg(Node, 197))
 }
 
 function ECCB_Plot(Node, EndPoint)
