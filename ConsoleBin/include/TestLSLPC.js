e36878244f2a1f46a8a5dbc7ef2ea9be8ecf1328
IvanovDmtr <d.ivanov@protonelectrotex.com>
2021-01-15 11:17:58 +0300

    Добавлены функции для работы с новым софтом блока, разработанный в True Studio

--- ConsoleBin/include/TestLSLPC.js
+++ ConsoleBin/include/TestLSLPC.js
@@ -158,6 +158,44 @@ function Print_FaultDisableWarning()
 }
 //------------------------------------------------------------
 
+// LSLPC 2.0
+function LSLPC_Start(Current)
+{
+	dev.w(128, Current * 10);
+	dev.c(100);
+	
+	while(dev.r(192) != 4)
+	{
+		sleep(50);
+		
+		if(dev.r(192) == 1)
+			PrintStatus();
+	}
+	
+	dev.c(101);
+	
+	sleep(20);
+	
+	while(dev.r(192) != 3)
+	{
+		sleep(50);
+		
+		if(dev.r(192) == 1)
+			PrintStatus();
+	}
+}
+//--------------------------
+
+function LSLPC_Pulses(Current, N)
+{
+	for(i = 0; i < N; i++)
+	{
+		print("#" + i);
+		LSLPC_Pulse(Current);
+	}
+}
+//--------------------------
+
 
 
 
