b4d61247a3ea5a83ff59ecc08d90f66beb6f0125
IvanovDmtr <d.ivanov@protonelectrotex.com>
2021-01-18 11:57:50 +0300

    Отлаженная бибилиотека калибровки LSLPC на софте, расположенный в git.

--- ConsoleBin/include/CalLSLPC.js
+++ ConsoleBin/include/CalLSLPC.js
@@ -1,244 +1,291 @@
-include("Tektronix.js");
 include("TestLSLPC.js")
+include("Tektronix.js")
 include("CalGeneral.js")
 
-//Константы
-TEK = 0;
-PICO = 1;
-//
+// Calibration setup parameters
+cal_Points = 10;
 
-//Переменные
-var Verify = 0;
-var Osc = PICO;
-//
+cal_Rshunt = 250;	// uOhm
 
+cal_CurrentRange = 1;
 
-//----------------------------------------------------------------------------------
-function LSLPC_Cal_I_Verify(LSLPC_Port, TekPort, Range)
+cal_IdMin = [100, 1000.1];	
+cal_IdMax = [1000, 6500];
+cal_IdStp = (cal_IdMax[cal_CurrentRange] - cal_IdMin[cal_CurrentRange]) / cal_Points;
+
+cal_Iterations = 1;
+cal_UseAvg = 1;
+//		
+
+// Counters
+cal_CntTotal = 0;
+cal_CntDone = 0;
+
+// Channels
+cal_chMeasureId = 1;
+cal_chSync = 3;
+
+// Results storage
+cal_Id = [];
+
+// Tektronix data
+cal_IdSc = [];
+
+// Relative error
+cal_IdErr = [];
+
+// Correction
+cal_IdCorr = [];
+
+function CAL_Init(portDevice, portTek, channelMeasureId)
 {
-	Verify = 1;
-	
-	LSLPC_Cal_I(LSLPC_Port, TekPort, Range);
-	
-	Verify = 0;
-}
-//----------------------------------------------------------------------------------
-
-//----------------------------------------------------------------------------------
-function LSLPC_Cal_I(LSLPC_Port, TekPort, Range)
-{	
-	//Переменные
-	var TEK_Itm = 0;
-	var Current = 0;
-	var dI = 0;
-	var Cal_Data = [];
-	var I_error = [];
-	var LSLPC_I = [];
-	var Current=0;
-	var Imin=0;
-	
-	//Константы
-	ITM_R_SHUNT					= 0.75;
-	LSLPC_I_MIN					= 200;
-	LSLPC_I_MAX					= 1500;
-	LSLPC_Range_THRESHOLD		= 1500;
-	N							= 10;
-	//
-	
-	//Сброс старых коэффициентов
-	if(Verify==0)
+	if (channelMeasureId < 1 || channelMeasureId > 4)
 	{
-		if(Range==0)
-		{
-			dev.w(4,0);
-			dev.w(5,0);
-		}
-		
-		if(Range==1)
-		{
-			dev.w(6,0);
-			dev.w(7,0);
-		}
-	}
-	//
-	
-	dev.Connect(LSLPC_Port);	
-	
-	if(Osc == TEK)
-	{
-		LSL_TekInit(TekPort);
-		LSL_TekCursor(1);
+		print("Wrong channel numbers");
+		return;
 	}
 
-	
-	if(Range==0)
-	{
-		Imin = LSLPC_I_MIN;
-		dI = parseFloat((LSLPC_Range_THRESHOLD-1-Imin)/(N-1)).toFixed(0);
-	}
-	
-	if(Range==1)
-	{
-		Imin = LSLPC_Range_THRESHOLD;
-		dI = parseFloat((LSLPC_I_MAX-Imin)/(N-1)).toFixed(0);
-	}
-	
-	for(i=0;i<N;i++)
-	{			
-		Current = parseInt(dI*i+Imin);
-	
-		if((Range==0)&&(Current>(LSLPC_Range_THRESHOLD-1)))
-		{
-			Current = LSLPC_Range_THRESHOLD-1;
-		}
-		
-		if((Range==1)&&(Current>LSLPC_I_MAX))
-		{
-			Current = LSLPC_I_MAX;
-		}
+	// Copy channel information
+	cal_chMeasureId = channelMeasureId;
 
-		//Смена вертикальный шкалы осциллографа
-		if(Osc == TEK)
-		{
-			LSL_TekRangeChange(Current,ITM_R_SHUNT);
-		}
-	
-		//Формируем импульс
-		LSLPC_Pulse(Current);
-		
-		if(Osc == PICO)
-		{
-			//Измеряем остальные параметры
-			print("Enter force value (in mV):");
-			PICO_Itm = Math.round(readline()/ITM_R_SHUNT);
-			print("----------------------------------");
+	// Init device port
+	dev.Disconnect();
+	dev.Connect(portDevice);
 
-			if (isNaN(PICO_Itm))
-			return;
-		}
-		
-		
-		if(Osc == TEK)
-		{
-			TEK_Itm = parseFloat(TEK_Exec("cursor:vbars:hpos1?")).toFixed(3)*1000/ITM_R_SHUNT;
-		}
-		//
-		
-		if(Osc == TEK)
-		{
-			I_error[i] = (Current-TEK_Itm)*100/TEK_Itm;
-			LSLPC_I[i] = TEK_Itm;
-			
-			//Сохраняем данные в файл
-			Cal_Data[i]=(TEK_Itm+";"+Current);
-			save("data/lsl_cal_seti.csv",Cal_Data);
-		}
+	// Init Tektronix port
+	TEK_PortInit(portTek);
+	
+	// Tektronix init
+	for (var i = 1; i <= 4; i++)
+	{
+		if (i == channelMeasureId)
+			TEK_ChannelOn(i);
 		else
-		{
-			I_error[i] = (Current-PICO_Itm)*100/PICO_Itm;
-			LSLPC_I[i] = PICO_Itm;
-			
-			//Сохраняем данные в файл
-			Cal_Data[i]=(PICO_Itm+";"+Current);
-			save("data/lsl_cal_seti.csv",Cal_Data);
-		}
-		//
+			TEK_ChannelOff(i);
 	}
+}
+//--------------------
+
+function CAL_CalibrateId()
+{		
+	CAL_ResetA();
+	CAL_ResetIdCal();
 	
-	if(Range==0)
+	// Tektronix init
+	CAL_TekInit(cal_chMeasureId);
+
+	// Reload values
+	var CurrentArray = CGEN_GetRange(cal_IdMin[cal_CurrentRange], cal_IdMax[cal_CurrentRange], cal_IdStp);
+
+	if (CAL_CollectId(CurrentArray, cal_Iterations))
 	{
-		scattern(LSLPC_I, I_error, "Current (in A)", "Error (in %)", "Current relative error (Range 0)");
+		CAL_SaveId("LSLPC_Id");
 
-		if(Verify==0)
-		{
-			//Вычисление корректировки
-			I_corr = CGEN_GetCorrection("lsl_cal_seti");
-			
-			//Сохранение калибровочных коэффициентов в память LSLH_Utm
-			dev.w(4,Math.round(I_corr[0]*1000));
-			dev.ws(5,Math.round(I_corr[1]));
-			dev.c(200);
-		}
+		// Plot relative error distribution
+		scattern(cal_IdSc, cal_IdErr, "Current (in uA)", "Error (in %)", "Current relative error");
+
+		// Calculate correction
+		cal_IdCorr = CGEN_GetCorrection2("LSLPC_Id");
+		CAL_SetCoefId(cal_IdCorr[0], cal_IdCorr[1], cal_IdCorr[2]);
+		CAL_PrintCoefId();
 	}
+}
+//--------------------
+
+function CAL_VerifyId()
+{		
+	CAL_ResetA();
 	
-	if(Range==1)
+	// Tektronix init
+	CAL_TekInit();
+
+	// Reload values
+	var CurrentArray = CGEN_GetRange(cal_IdMin[cal_CurrentRange], cal_IdMax[cal_CurrentRange], cal_IdStp);
+
+	if (CAL_CollectId(CurrentArray, cal_Iterations))
 	{
-		scattern(LSLPC_I, I_error, "Current (in A)", "Error (in %)", "Current relative error (Range 1)");
+		CAL_SaveId("LSLPC_Id_fixed");
 
-		if(Verify==0)
-		{
-			//Вычисление корректировки
-			I_corr = CGEN_GetCorrection("lsl_cal_seti");
-			
-			//Сохранение калибровочных коэффициентов в память LSLH_Utm
-			dev.w(6,Math.round(I_corr[0]*1000));
-			dev.ws(7,Math.round(I_corr[1]));
-			dev.c(200);
-		}
+		// Plot relative error distribution
+		scattern(cal_IdSc, cal_IdErr, "Current (in uA)", "Error (in %)", "Current relative error");
 	}
 }
-//----------------------------------------------------------------------------------
+//--------------------
 
-
-
-
-
-//----------------------------------------------------------------------------------
-function LSL_TekRangeChange(Current, Rshunt)
+function CAL_CollectId(CurrentValues, IterationsCount)
 {
-	//Смена вертикальный шкалы осциллографа
-	Tek_Vmax = Current*Rshunt/1000;
-	Tek_V_Range = Tek_Vmax/7;
+	cal_CntTotal = IterationsCount * CurrentValues.length;
+	cal_CntDone = 1;
+
+	var AvgNum;
+	if (cal_UseAvg)
+	{
+		AvgNum = 4;
+		TEK_AcquireAvg(AvgNum);
+	}
+	else
+	{
+		AvgNum = 1;
+		TEK_AcquireSample();
+	}
 	
-	if(Tek_V_Range<=0.002){LSL_TEK_ChannelInit(1, 1, 0.002);}
-	if((Tek_V_Range>0.002)&&(Tek_V_Range<=0.005)){LSL_TEK_ChannelInit(1, 1, 0.005);}
-	if((Tek_V_Range>0.005)&&(Tek_V_Range<=0.01)){LSL_TEK_ChannelInit(1, 1, 0.01);}
-	if((Tek_V_Range>0.01)&&(Tek_V_Range<=0.02)){LSL_TEK_ChannelInit(1, 1, 0.02);}
-	if((Tek_V_Range>0.02)&&(Tek_V_Range<=0.05)){LSL_TEK_ChannelInit(1, 1, 0.05);}
-	if((Tek_V_Range>0.05)&&(Tek_V_Range<=0.1)){LSL_TEK_ChannelInit(1, 1, 0.1);}
-	if((Tek_V_Range>0.1)&&(Tek_V_Range<=0.2)){LSL_TEK_ChannelInit(1, 1, 0.2);}
-	if((Tek_V_Range>0.2)&&(Tek_V_Range<=0.5)){LSL_TEK_ChannelInit(1, 1, 0.5);}
-	if((Tek_V_Range>0.5)&&(Tek_V_Range<=1)){LSL_TEK_ChannelInit(1, 1, 1);}
-	if((Tek_V_Range>1)&&(Tek_V_Range<=2)){LSL_TEK_ChannelInit(1, 1, 2);}
-	if((Tek_V_Range>2)&&(Tek_V_Range<=5)){LSL_TEK_ChannelInit(1, 1, 5);}
-	if(Tek_V_Range>5){LSL_TEK_ChannelInit(1, 1, 5);}
+	for (var i = 0; i < IterationsCount; i++)
+	{
+		for (var j = 0; j < CurrentValues.length; j++)
+		{
+			print("-- result " + cal_CntDone++ + " of " + cal_CntTotal + " --");
+			//
+			LSLPC_TekScale(cal_chMeasureId, CurrentValues[j] * cal_Rshunt / 1000000);
+			sleep(1000);
+			
+			for (var k = 0; k < AvgNum; k++)
+			{
+				sleep(500);
+				if(!LSLPC_Start(CurrentValues[j]))
+					return false;
+			}
+			
+			// Unit data
+			var IdSet = dev.r(128) / 10;
+			cal_Id.push(IdSet);
+			print("Idset, A: " + IdSet);
+
+			// Scope data
+			var IdSc = (CAL_Measure(cal_chMeasureId) / cal_Rshunt * 1000000).toFixed(2);
+			cal_IdSc.push(IdSc);
+			print("Idtek, A: " + IdSc);
+
+			// Relative error
+			var IdErr = ((IdSet - IdSc) / IdSc * 100).toFixed(2);
+			cal_IdErr.push(IdErr);
+			print("Iderr, %: " + IdErr);
+			print("--------------------");
+			
+			if (anykey()) return 0;
+		}
+	}
+
+	return 1;
+}
+//--------------------
+
+function LSLPC_TekScale(Channel, Value)
+{
+	Value = Value / 7;
+	TEK_Send("ch" + Channel + ":scale " + Value);
 	
-	sleep(1000);
-	//
+	TEK_TriggerPulseInit(cal_chMeasureId, Value * 3);
 }
-//----------------------------------------------------------------------------------
+//--------------------
 
-//----------------------------------------------------------------------------------
-function LSL_TEK_ChannelInit(Channel, Probe, Scale)
+function CAL_TekInit()
 {
-	TEK_Send("ch" + Channel + ":bandwidth on");
-	TEK_Send("ch" + Channel + ":coupling dc");
-	TEK_Send("ch" + Channel + ":invert off");
-	TEK_Send("ch" + Channel + ":position -4");
-	TEK_Send("ch" + Channel + ":probe " + Probe);
-	TEK_Send("ch" + Channel + ":scale " + Scale);
+	TEK_ChannelInit(cal_chMeasureId, "1", "0.01");
+	TEK_TriggerPulseInit(cal_chMeasureId, "0.04");
+	TEK_Horizontal("0.5e-3", "-2e-3");
+	TEK_Send("MEASurement:MEAS" + cal_chMeasureId + ":TYPe MAXImum");
 }
-//----------------------------------------------------------------------------------
+//--------------------
 
-//----------------------------------------------------------------------------------
-function LSL_TekInit(Port)
+function CAL_TekScale(Channel, Value)
 {
-	//Tektronix config
-	TEK_PortInit(Port);
-	LSL_TEK_ChannelInit(1, 1, 0.2);//(channel,probe,scale) I dut
-	LSL_TEK_ChannelInit(2, 1, 0.5);//(channel,probe,scale) U dut
-	LSL_TEK_ChannelInit(3, 1, 2);//(channel,probe,scale) sync
-	TEK_TriggerInit(3, 2.5);//(channel,level) sync
-	TEK_Horizontal(2.5e-3, 0);//(scale,position)
+	Value = Value / 6;
+	TEK_Send("ch" + Channel + ":scale " + Value);
 }
-//----------------------------------------------------------------------------------
+//--------------------
 
-
-//----------------------------------------------------------------------------------
-function LSL_TekCursor(Channel)
+function CAL_Measure(Channel)
 {
-	TEK_Send("cursor:select:source ch" + Channel);
-	TEK_Send("cursor:function vbars");
-	TEK_Send("cursor:vbars:position1 0");
+	return TEK_Measure(Channel);
 }
-//----------------------------------------------------------------------------------
+//--------------------
+
+function CAL_ResetA()
+{	
+	// Results storage
+	cal_Id = [];
+
+	// Tektronix data
+	cal_IdSc = [];
+
+	// Relative error
+	cal_IdErr = [];
+
+	// Correction
+	cal_IdCorr = [];
+}
+//--------------------
+
+function CAL_SaveId(NameId)
+{
+	CGEN_SaveArrays(NameId, cal_Id, cal_IdSc, cal_IdErr);
+}
+//--------------------
+
+function CAL_ResetIdCal()
+{
+	CAL_SetCoefId(0, 1, 0);
+}
+//--------------------
+
+function CAL_SetCoefId(P2, P1, P0)
+{
+	switch(cal_CurrentRange)
+	{	
+		case 0:
+		{
+			dev.ws(31, Math.round(P2 * 1e6));
+			dev.w(32, Math.round(P1 * 1000));
+			dev.ws(33, Math.round(P0) * 10);
+		}
+		break;
+		
+		case 1:
+		{
+			dev.ws(37, Math.round(P2 * 1e6));
+			dev.w(38, Math.round(P1 * 1000));
+			dev.ws(39, Math.round(P0) * 10);
+		}
+		break;
+		
+		case 2:
+		{
+			dev.ws(43, Math.round(P2 * 1e6));
+			dev.w(44, Math.round(P1 * 1000));
+			dev.ws(45, Math.round(P0) * 10);
+		}
+		break;
+	}		
+}
+//--------------------
+
+function CAL_PrintCoefId()
+{
+	switch(cal_CurrentRange)
+	{
+		case 0:
+		{
+			print("Id 0 P2 x1e6		: " + dev.rs(31));
+			print("Id 0 P1 x1000	: " + dev.rs(32));
+			print("Id 0 P0 x10		: " + dev.rs(33));
+		}
+		break;
+		
+		case 1:
+		{
+			print("Id 1 P2 x1e6		: " + dev.rs(37));
+			print("Id 1 P1 x1000	: " + dev.rs(38));
+			print("Id 1 P0 x10		: " + dev.rs(39));
+		}
+		break;
+		
+		case 2:
+		{
+			print("Id 2 P2 x1e6		: " + dev.rs(43));
+			print("Id 2 P1 x1000	: " + dev.rs(44));
+			print("Id 2 P0 x10		: " + dev.rs(45));
+		}
+		break;
+	}
+}
+//--------------------
