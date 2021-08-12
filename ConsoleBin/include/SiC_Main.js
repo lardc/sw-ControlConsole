d1691a36f6d28fcc63cbdcab2929fc33bb7f7db3
asytyi <alexander.a.sytyi@gmail.com>
2021-08-12 17:54:14 +0300

    SiC Размерности результатов приведены к целым числам

--- ConsoleBin/include/SiC_Main.js
+++ ConsoleBin/include/SiC_Main.js
@@ -22,29 +22,29 @@ function SiC_Main(Curves)
 	var Mode = SiC_CALC_OnMode(Curves);
 	var RiseFallData = SiC_CALC_VI_RiseFall(Curves);
 	
-	print("V" + ":\t\t" + RiseFallData.V_points.S_amp.toFixed(1) + "\t (V)");
-	print("Vmax" + ":\t\t" + RiseFallData.V_points.S_max.toFixed(1) + "\t (V)");
+	print("V" + ":\t\t" + RiseFallData.V_points.S_amp.toFixed(0) + "\t (V)");
+	print("Vmax" + ":\t\t" + RiseFallData.V_points.S_max.toFixed(0) + "\t (V)");
 	print("---");
-	print("I" + ":\t\t" + RiseFallData.I_points.S_amp.toFixed(1) + "\t (A)");
-	print("Imax" + ":\t\t" + RiseFallData.I_points.S_max.toFixed(1) + "\t (A)");
+	print("I" + ":\t\t" + RiseFallData.I_points.S_amp.toFixed(0) + "\t (A)");
+	print("Imax" + ":\t\t" + RiseFallData.I_points.S_max.toFixed(0) + "\t (A)");
 	print("---");
-	print("dI/dt_" + (Mode ? "on" : "off") + ":\t" + RiseFallData.I_points.S_rf.toFixed(1) + "\t (A/us)");
-	print("t" + (Mode ? "r" : "f") + "i:\t\t" + RiseFallData.I_points.t_rf.toFixed(1) + "\t (ns)");
-	print("tdi_" + (Mode ? "on: " : "off:") + "\t" + SiC_CALC_Delay(Curves).toFixed(1) + "\t (ns)");
+	print("dI/dt_" + (Mode ? "on" : "off") + ":\t" + RiseFallData.I_points.S_rf.toFixed(0) + "\t (A/us)");
+	print("t" + (Mode ? "r" : "f") + "i:\t\t" + RiseFallData.I_points.t_rf.toFixed(0) + "\t (ns)");
+	print("tdi_" + (Mode ? "on: " : "off:") + "\t" + SiC_CALC_Delay(Curves).toFixed(0) + "\t (ns)");
 	print("---");
-	print("dU/dt_" + (Mode ? "on" : "off") + ":\t" + RiseFallData.V_points.S_rf.toFixed(1) + "\t (V/us)");
-	print("t" + (Mode ? "f" : "r") + "v:\t\t" + RiseFallData.V_points.t_rf.toFixed(1) + "\t (ns)");
-	print("E_" + (Mode ? "on" : "off") + ":\t\t" + SiC_CALC_Energy(Curves).Energy.toFixed(3) + "\t (mJ)");
+	print("dU/dt_" + (Mode ? "on" : "off") + ":\t" + RiseFallData.V_points.S_rf.toFixed(0) + "\t (V/us)");
+	print("t" + (Mode ? "f" : "r") + "v:\t\t" + RiseFallData.V_points.t_rf.toFixed(0) + "\t (ns)");
+	print("E_" + (Mode ? "on" : "off") + ":\t\t" + SiC_CALC_Energy(Curves).Energy.toFixed(0) + "\t (mJ)");
 	
 	if (Mode)
 	{
 		var Recovery = SiC_CALC_Recovery(Curves);
 		
 		print("---");
-		print("Irrm" + ":\t\t" + Recovery.Irrm.toFixed(1) + "\t (A)");
-		print("trr" + ":\t\t" + Recovery.trr.toFixed(1) + "\t (ns)");
-		print("Qrr" + ":\t\t" + Recovery.Qrr.toFixed(3) + "\t (uC)");
-		print("E_rec" + ":\t\t" + Recovery.Energy.toFixed(3) + "\t (mJ)");
+		print("Irrm" + ":\t\t" + Recovery.Irrm.toFixed(0) + "\t (A)");
+		print("trr" + ":\t\t" + Recovery.trr.toFixed(0) + "\t (ns)");
+		print("Qrr" + ":\t\t" + Recovery.Qrr.toFixed(0) + "\t (uC)");
+		print("E_rec" + ":\t\t" + Recovery.Energy.toFixed(1) + "\t (mJ)");
 	}
 }
 
