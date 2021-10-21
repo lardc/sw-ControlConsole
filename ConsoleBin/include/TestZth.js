c0b8ed8f8a86e85cf51ce69dfa3007688bb2d8e7
d.ivanov <d.ivanov@proton-electrotex.com>
2021-10-21 10:32:05 +0300

    Добавлены фкнции запуска Ih, Im, Ig в Zth

--- ConsoleBin/include/TestZth.js
+++ ConsoleBin/include/TestZth.js
@@ -1,52 +1,94 @@
 include("PrintStatus.js")
-include("Tektronix.js")
-include("CalGeneral.js")
 
-arr_v = [];
-
-function ADC()	// Single measurement 
+function Zth_Im(Current, PulseWidth_us)
 {
-	dev.c(6);
-	a = dev.r(10);
-	b = 5000*a/65536;
-	print("Uin_adc [mV] = " + b);
+	var PulseWidth_L, PulseWidth_H;
+	var Sleep;
 	
-	return;
-}
-
-function ADC_Collect(Num, Sleep)		// Serial measurement
-{
-	for (i = 0; i < Num; i++)
+	Sleep = PulseWidth_us / 1e6;
+	if(Sleep < 1)
+		Sleep = 1;
+	
+	PulseWidth_L = PulseWidth_us & 0xFFFF;
+	PulseWidth_H = (PulseWidth_us >> 16) & 0xFFFF;
+	
+	dev.w(135, PulseWidth_L);
+	dev.w(136, PulseWidth_H);
+	dev.w(139, Current);
+	dev.w(128, 4);
+	dev.c(50);
+	p("Process...");
+	
+	for(i = 0; i < Sleep; i++)
 	{
-		dev.c(6);
-		arr_v[i] = dev.r(10);
-		sleep(Sleep);
-		if (anykey()) return;
+		sleep(1000);
+		p("Im, mA: " + dev.r(203) / 10);
+		
+		if(anykey())
+		{
+			dev.c(101);
+			return;
+		}
+		
+		if(dev.r(192) != 5)
+			return;
 	}
-	for (i = 0; i < Num; i++)
+}
+//------------------------------
+
+function Zth_Gate(Type, Value, State)
+{	
+	dev.w(129, Type);
+	
+	if(Type)
 	{
-		b = 5000*arr_v[i]/65536;
-		arr_v[i] = b;
+		if(Value)
+			dev.w(143, 1);
+		else
+			dev.w(143, 0);
+	}
+	else
+		dev.w(139, Value);
+
+	if(State)
+		dev.c(52);
+	else
+		dev.c(101);
+}
+//------------------------------
+
+function Zth_Ih(Current, PulseWidth_us)
+{
+	var PulseWidth_L, PulseWidth_H;
+	var Sleep;
+	
+	Sleep = PulseWidth_us / 1e6;
+	if(Sleep < 1)
+		Sleep = 1;
+	
+	PulseWidth_L = PulseWidth_us & 0xFFFF;
+	PulseWidth_H = (PulseWidth_us >> 16) & 0xFFFF;
+	
+	dev.w(135, PulseWidth_L);
+	dev.w(136, PulseWidth_H);
+	dev.w(139, Current);
+	dev.w(128, 5);
+	dev.c(51);
+	p("Process...");
+	
+	for(i = 0; i < Sleep; i++)
+	{
+		sleep(1000);
+		p("#" + i + " Ih, A: " + dev.r(201) / 10);
+		
+		if(anykey())
+		{
+			dev.c(101);
+			return;
+		}
+		
+		if(dev.r(192) != 5)
+			return;
 	}
 }
-
-function ADC_Save(NameV)
-{
-	SaveArray(NameV, arr_v);
-}
-
-function ClrArray()
-{
-	for (var i = 0; i < arr_v.length; i++)
-	arr_v[i] = 0;
-}
-
-function SaveArray(FileName, UnitData)
-{
-	var csv_array = [];
-	
-	for (var i = 0; i < UnitData.length; i++)
-		csv_array.push(UnitData[i] + ";");
-	
-	save("TestZthDir/" + FileName + ".csv", csv_array);
-}
+//------------------------------
