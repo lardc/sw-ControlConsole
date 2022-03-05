51db8b2bb0f8ab76da776e95a5bf483e1970ca3c
AlexanrIg <CJDruid@mail.ru>
2022-03-05 18:43:04 +0300

    Обнавлен TestCSM

--- ConsoleBin/include/TestCSM.js
+++ ConsoleBin/include/TestCSM.js
@@ -18,3 +18,34 @@ function CSM_Pos(Distance)
 		PrintStatus()
 	}
 }
+function CSM_PosAdap (DeviceCase)
+
+{
+	dev.w(71,DeviceCase);
+	dev.c(110);
+	sleep(1000);
+	dev.c(102);
+	do
+	{
+		if(anykey())
+			return
+		sleep(100)
+	}
+	while(dev.r(96) == 7)
+	
+	if(dev.r(96) != 8)
+	{
+		print("Bad status")
+		PrintStatus()
+	}
+}
+
+function CSM_UNClamp ()
+
+{
+	dev.c(120);
+	sleep(1000);
+	dev.c(109);
+	sleep(100);
+
+}
