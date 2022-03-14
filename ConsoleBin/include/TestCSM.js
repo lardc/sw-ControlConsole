6855801ff9bf3faf6c15132f169d9e526a7618c4
mlhzv2204 <rsln.mlhzv@gmail.com>
2022-03-14 16:12:17 +0300

    Скорректирован вывод данных при измерении темп

--- ConsoleBin/include/TestCSM.js
+++ ConsoleBin/include/TestCSM.js
@@ -141,14 +141,15 @@ function CS_Temp(Sleep, Temp)
 		dev.w(84, 1);
 
 		sleep(Sleep);
-		csm_csv_array.push(cs_time[i] + ";" + cs_t_remote1[i] + ";" + cs_t_remote_ext[i]);
+		csm_csv_array.push("Время в сек ; Температура внутр ТРМ; Температура внешн ТРМ");
+		csm_csv_array.push(i * Sleep / 1000 + ";" + cs_t_remote1[i] + ";" + cs_t_remote_ext[i]);
 
 		i++;
 	}
 	//---------------------------------------
 
 	plot2s(cs_t_remote1, cs_t_remote_ext, 10, 0);
-	save("data/CSM_TempTest" + today.getTime() + ".csv", csm_csv_array);
+	save("data/CSM_TempTest.csv", csm_csv_array);
 
 	//---------------------------------------
 }
