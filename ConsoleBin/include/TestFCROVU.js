a4f0c457a032477fe335a4a764f4ca516d57bbcd
mlhzv2204 <r.malkhozov@proton-electrotex.com>
2023-06-16 16:12:51 +0300

    Команда запуска сигнала от dVdt и кол-ва плат

--- ConsoleBin/include/TestFCROVU.js
+++ ConsoleBin/include/TestFCROVU.js
@@ -1,10 +1,23 @@
 include("PrintStatus.js")
 include("CalGeneral.js")
 
-
 function fdVdt_DiagPulse(Gate)
 {
-	dev.w(128, Gate);
-	dev.c(10);
+	dev.w(151, Gate);
+	sleep(50);
+	dev.c(120);
+	sleep(50);
 	dev.c(129);
+}
+
+function fdVdt_StartPulse(Rate, Current)
+{
+	dev.w(129, Rate * 10);
+	dev.c(10);
+	if(Current == 1)
+		dev.c(121);
+	else if (Current == 2)
+		dev.c(122);
+	else if (Current == 3)
+		dev.c(123);
 }
